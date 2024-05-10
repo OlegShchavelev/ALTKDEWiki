6ee2d605fd65347e770d170e7593f40502c3bc43
Ampernic <ampernic@inbox.ru>
2024-05-10 18:18:41 +0300

    Очередные правки виджета "Участники" в слепую

---
+++ .vitepress/theme/components/AKWHomeTeam.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {
+  VPTeamPage,
+  VPTeamPageTitle,
+  VPButton,
+} from 'vitepress/theme'
+
+import AKWHomeTeamButton from './AKWHomeTeamButton.vue';
+import AKWHomeTeamMembers from './AKWHomeTeamMembers.vue';
+import AKWTeamLoader from './AKWTeamLoader.vue';
+
+</script>
+
+<template>
+    <VPTeamPage>
+        <VPTeamPageTitle>
+            <template #title>
+                Участники
+            </template>
+        </VPTeamPageTitle>
+        <Suspense>
+            <AKWHomeTeamMembers/>
+            <template #fallback>
+                <AKWTeamLoader />
+            </template>
+        </Suspense>
+        <AKWHomeTeamButton>
+            <VPButton text="Все участники" class="button" size="big" href="/project/contributions/" />
+        </AKWHomeTeamButton>
+    </VPTeamPage>
+</template>
