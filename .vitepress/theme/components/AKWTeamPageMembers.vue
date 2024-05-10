8f58be50c7086002052966e15dc34a4ee07c7b44
Ampernic <ampernic@inbox.ru>
2024-05-10 20:51:15 +0300

    Правки виджета "Участники"

--- .vitepress/theme/components/AKWTeamPageMembers.vue
+++ .vitepress/theme/components/AKWTeamPageMembers.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { computed } from 'vue'
 import { VPTeamPage, VPTeamPageTitle, VPTeamMembers } from 'vitepress/theme'
 
 import { contributions, enable_autosearch, page_filter_type } from '../../data/team';
