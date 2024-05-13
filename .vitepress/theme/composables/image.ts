f6cdebc38b6059ea10f0322a0af5b641d6716d0f
Ampernic <ampernic@inbox.ru>
2024-05-14 02:47:07 +0300

    quickfix: assetImage - fix url generation

--- .vitepress/theme/composables/image.ts
+++ .vitepress/theme/composables/image.ts
@@ -5,7 +5,7 @@ export const assetImage = (url: string, path: string) => {
     if (!url) return
     if (isValidUrl(url) != false) return url
 
-    return (url[0] === './') ? new URL(`/${path.slice(1) + url.slice(2)}`, import.meta.url).href : url
+    return (String(url).slice(0, 2) === './') ? new URL(`/${path.slice(1) + url.slice(2)}`, import.meta.url).href : url
 
 }
 
