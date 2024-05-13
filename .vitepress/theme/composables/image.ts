ef051ab5112a97c0e146f83d9a53f69966b84b83
Oleg Shchavelev <o.shchavelev@yandex.ru>
2024-05-13 03:00:10 +0300

    components: update return function assetImage in template AKWDocsAsideMeta #9

--- .vitepress/theme/composables/image.ts
+++ .vitepress/theme/composables/image.ts
@@ -5,7 +5,7 @@ export const assetImage = (url: string, path: string) => {
     if (!url) return
     if (isValidUrl(url) != false) return url
 
-    return new URL(`${path + url.slice(2)}`, import.meta.url).href
+    return new URL(`/${path.slice(1) + url.slice(2)}`, import.meta.url).href
 
 }
 
