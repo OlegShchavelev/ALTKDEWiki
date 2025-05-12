1d3cfe0ba0a7abf7172a56606f50556f3a997c48
Олег Ананьев <aklemator@yandex.ru>
2025-05-12 10:51:40 +0300

    Revert "Изменение  структуры расположения ресурсов статей ПО, внесение изменений в sidebar_docs согласно новой структуры (разбите программ по категориям)"
    This reverts commit dbc588b725b29ec1cdc4d488cf9719889fcf38d6.

--- .vitepress/data/navigations.ts
+++ .vitepress/data/navigations.ts
@@ -15,118 +15,58 @@ const sidebar_docs = [
   {
     text: 'Установка и обновление программ',
     base: '/apps/',
-    items: [
-        {
-          text: 'Разработка',
-          items: [
-            { text: 'Plan', link: '/devel/plan/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'ЭЦП',
-          items: [
-            { text: 'КриптоПро CSP 5', link: '/eds/cryptopro/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Файловые менеджеры',
-          items: [
-            { text: 'Krusader', link: '/filemanagers/krusader/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Игры',
-          items: [
-        {
-          text: 'Эмуляторы',
-          items: [
-        { text: 'PCSX2', link: '/games/emulators/pcsx2/' }
-        ],
-            collapsed: true
-        },
-          { text: 'Steam', link: '/games/steam/' }
-        ],
-          collapsed: true
-        },
-        {
-          text: 'Графика',
-          items: [
-        { text: 'Flameshot', link: '/graphics/flameshot/' }
-         ],
-          collapsed: true
-        },
-        {
-          text: 'Мультимедиа',
-          items: [
-        { text: 'Haruna', link: '/multimedia/haruna/' }
-         ],
-           collapsed: true
-        },
-        {
-          text: 'Сеть',
-          items: [
-            { text: 'Audiotube', link: '/net/audiotube/' },
-            { text: 'Plasmatube', link: '/net/plasmatube/' },
-            { text: 'qBittorrent', link: '/net/qbittorrent/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Офис',
-          items: [
-            { text: 'Arianna', link: '/office/arianna/' },
-            { text: 'Kiten', link: '/office/kiten/' },
-            { text: 'Klevernotes', link: '/office/klevernotes/' },
-            { text: 'LibreOffice', link: '/office/libreoffice/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Безопасность',
-          items: [
-            { text: 'KeePassXC', link: '/security/keepassxc/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Система',
-          items: [
-            { text: 'Conky', link: '/system/conky/' }
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Инструменты',
-          items: [
-            {
-              text: 'Разное',
-              items: [
-                { text: 'SafeEyes', link: '/utilities/different/safeeyes/' }
-              ],
-              collapsed: true
-            },
-            {
-              text: 'Текстовые редакторы',
-              items: [
-                { text: 'Micro', link: '/utilities/texteditors/micro/' },
-                { text: 'Nano', link: '/utilities/texteditors/nano/' }
-              ],
-              collapsed: true
-            },
-          ],
-          collapsed: true
-        },
-        {
-          text: 'Виртуализация, контейнеризация',
-          items: [
-            { text: 'Distrobox', link: '/vm-container/distrobox/' }
-          ],
-          collapsed: true
-        },
-  ],
+    items: [{
+      text: 'Arianna',
+      link: 'arianna/'
+    }, {
+      text: 'Distrobox',
+      link: 'distrobox/'
+    }, {
+      text: 'КриптоПро CSP 5',
+      link: 'cryptopro/'
+    }, {
+      text: 'Flameshot',
+      link: 'flameshot/'
+    }, {
+      text: 'Haruna',
+      link: 'haruna/'
+    }, {
+      text: 'KeePassXC',
+      link: 'keepassxc/'
+    }, {
+      text: 'KleverNotes',
+      link: 'klevernotes/'
+    }, {
+      text: 'Krusader',
+      link: 'krusader/'
+    }, {
+      text: 'LibreOffice',
+      link: 'libreoffice/'
+    }, {
+      text: 'Micro',
+      link: 'micro/'
+    }, {
+      text: 'Nano',
+      link: 'nano/'      
+    }, {
+      text: 'PCSX2',
+      link: 'pcsx2/'
+    }, {
+      text: 'Plan',
+      link: 'plan/'
+    }, {
+      text: 'PlasmaTube',
+      link: 'plasmatube/'
+    }, {
+      text: 'qBittorrent',
+      link: 'qbittorrent/'
+    }, {
+      text: 'SafeEyes',
+      link: 'safeeyes'
+    }, {
+      text: 'Steam',
+      link: 'steam/'
+    }],
     collapsed: false
   },
   {
