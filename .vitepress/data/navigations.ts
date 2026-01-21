b19bfbb8bd811568f154ab4eb7af6d945f7fe750
SLIMMIND <slimmind@gmail.com>
2026-01-21 13:08:05 +0300

    Исправлено: ссылка на пост об Krunner добавлена в навигацию

--- .vitepress/data/navigations.ts
+++ .vitepress/data/navigations.ts
@@ -150,6 +150,10 @@ const sidebar_docs = [
       {
         text: 'Настройка ночной цветовой схемы (Night Light)',
         link: '/night-light/'
+      },
+      {
+        text: 'KRunner в KDE Plasma 6',
+        link: '/krunner/'
       }
     ],
     collapsed: true
